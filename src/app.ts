b20986c0dbc6401f2e2b78e590696b941818c250
rohith <rohithvudayana46@gmail.com>
2024-06-09 13:09:29 +0530

    added app

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import { httpResponse } from "./helpers/createResponse";
+import { routeNotFound } from "./middleware/routeNotFound";
+import { errorHandler } from "./middleware/errorHandler";
+import { authRouter } from "./routes/authRoutes";
+import {reviewRouter} from "./routes/reviewRoutes";
+import { breweRouter } from "./routes/breweRoutes";
+import cors from "cors";
+
+dotenv.config();
+
+export const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/ok", (_req, _res) => {   _res.status(200).send(httpResponse(true, "OK", {}))   })
+
+app.use(`/auth`, authRouter);
+app.use(`/review`, reviewRouter);
+app.use(`/brewery`, breweRouter);
+
+app.use(cors({}))
+app.use(routeNotFound);
+app.use(errorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,15 @@
-import express from "express";
-import dotenv from "dotenv";
 import { connectDB } from "./database/database";
-import { httpResponse } from "./helpers/createResponse";
-import { routeNotFound } from "./middleware/routeNotFound";
-import { errorHandler } from "./middleware/errorHandler";
-import { authRouter } from "./routes/authRoutes";
-import {reviewRouter} from "./routes/reviewRoutes";
-import { breweRouter } from "./routes/breweRoutes";
-import cors from "cors";
-
-dotenv.config();
-
-const app = express();
-
+import { app } from "./app";
 const port = process.env.PORT || 4000;
-try{
-    if(!process.env.CONNECTION)
-        throw new Error("no connection string found in .env file");
 
-    connectDB(process.env.CONNECTION);
-    app.listen(port, () => {
-        console.log(`server listening on : http://localhost:${port}/`);
-    });
+try {
+  if (!process.env.CONNECTION)
+    throw new Error("No connection string found in .env file");
+  connectDB(process.env.CONNECTION);
+
+  app.listen(port, () => {
+    console.log(`Server listening on: http://localhost:${port}/`);
+  });
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/ok", (_req, _res) => {   _res.status(200).send(httpResponse(true, "OK", {}))   })
-
-app.use(`/auth`, authRouter);
-app.use(`/review`, reviewRouter);
-app.use(`/brewery`, breweRouter);
-
-app.use(cors({}))
-app.use(routeNotFound);
-app.use(errorHandler);
-
